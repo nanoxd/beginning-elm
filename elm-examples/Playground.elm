bfa1a4e8e1fa3933b315c34760e14c85aecfffaa
Fernando Paredes <nano@fdp.io>
2016-12-18 21:15:45 -0800

    [Ch 3.15] Try out regexes

--- elm-examples/Playground.elm
+++ elm-examples/Playground.elm
@@ -1,6 +1,7 @@
 module Playground exposing (..)
 
 import Html
+import Regex
 
 
 escapeEarth velocity speed fuelStatus =
@@ -129,7 +130,20 @@ revelation =
     """
 
 
+apollo =
+    """
+    On July 16, 1969, the massive Saturn V rocket lifted
+    off from NASA's Kennedy Space Center at 09:32 a.m. EDT.
+    Four days later, on July 20, Neil Armstrong and Buzz Aldrin
+    landed on the Moon.
+    """
+
+
+launchTimePattern =
+    Regex.regex "\\d\\d:\\d\\d (a\\.m\\.|p\\.m\\.)"
+
+
 main =
-    String.length revelation
+    Regex.contains launchTimePattern apollo
         |> toString
         |> Html.text
