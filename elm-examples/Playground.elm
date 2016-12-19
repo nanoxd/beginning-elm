75f395ea62c1701c9377756732111f70ed361927
Fernando Paredes <nano@fdp.io>
2016-12-18 20:57:24 -0800

    [Ch 3.14] Add a multiline string

--- elm-examples/Playground.elm
+++ elm-examples/Playground.elm
@@ -118,6 +118,17 @@ whereToPark speed =
             "Nowhere"
 
 
+revelation =
+    """
+    It became very clear to me sitting out there today
+    that every decision I've made in my entire life has
+    been wrong. My life is the complete "opposite" of
+    everything I want it to be. Every instinct I have,
+    in every aspect of life, be it something to wear,
+    something to eat - it's all been wrong.
+    """
+
+
 main =
-    whereToPark 7.67
+    revelation
         |> Html.text
